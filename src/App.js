01a5b289674900411c2dfd2bd8accd78bd96704f
sridhanush31 <97938428+sridhanush31@users.noreply.github.com>
2022-02-25 16:01:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Appbar from './components/appbar';
+import Student from './components/Student';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Appbar/> 
+    <Student/> 
+    </div>
+  );
+}
+
+export default App;
